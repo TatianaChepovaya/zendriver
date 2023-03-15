fc37e9907967ce7da954b2ac90f0f590ed1361e5
TatianaChepovaya <taniacheport@gmail.com>
2023-03-15 18:26:23 +0200

    .

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
-    $('.reviews__slider').slick({
-      slidesToShow: 3,
-      slidesToScroll: 1,
-      autoplay: true,
-      autoplaySpeed: 2000,
-      dots: true,
-    })
+  $('.reviews__slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots: true,
   })
+})
