2a62cd114e9a2a35cb05e8a6a1bfa9a570ebc93c
TatianaChepovaya <taniacheport@gmail.com>
2023-03-16 19:51:52 +0200

    finish

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 $(document).ready(function () {
   $('.reviews__slider').slick({
-    slidesToShow: 1,
+    slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000,
     dots: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ],
   })
+})
+
+$(document).ready(function () {
+  if ($(window).width() < 768) {
+    $('.library__btn').appendTo('.library__container')
+    console.log('1024')
+  }
 })
