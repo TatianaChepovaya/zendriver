c74b37f4becd12e1a57e7eadbea08936f22d3daf
TatianaChepovaya <taniacheport@gmail.com>
2023-03-17 12:27:33 +0200

    menu

--- menu-mob.js
+++ menu-mob.js
@@ -1,8 +1,12 @@
 $(document).ready(function () {
   //   console.log("ready!");
-  $(".mobile__menu-btn").on("click", function () {
+  $('.mobile__menu-btn').on('click', function () {
     //  alert();
-    $(".menu").toggleClass("active");
-    $(".mobile__menu-btn").toggleClass("active");
-  });
-});
+    $('.mobile__menu-btn').toggleClass('active')
+    $('.header__main').toggleClass('active')
+    $('.header__line').toggleClass('active')
+    $('.mobile__menu-container').toggleClass('active')
+    $('.menu').toggleClass('active')
+    $('.menu').prependTo('.mobile__menu-container .container')
+  })
+})
